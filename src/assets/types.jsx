6e28994823f354626920df49cf632e64b571ff40
Alan Desktop <albertodro0909@gmail.com>
2023-02-10 09:40:36 -0600

    Add types constants

---
+++ src/assets/types.jsx
@@ -0,0 +1,43 @@
+import { BugBadge, DarkBadge, DragonBadge, ElectricBadge, FairyBadge, FightingBadge, FireBadge, FlyingBadge, GhostBadge, GrassBadge, GroundBadge, IceBadge, NormalBadge, PoisonBadge, PsychicBadge, RockBadge, SteelBadge, WaterBadge } from './icons/Badges'
+
+export const colors = {
+  normal: '#A8A77A',
+  fire: '#EE8130',
+  water: '#6390F0',
+  electric: '#F7D02C',
+  grass: '#7AC74C',
+  ice: '#96D9D6',
+  fighting: '#C22E28',
+  poison: '#A33EA1',
+  ground: '#E2BF65',
+  flying: '#A98FF3',
+  psychic: '#F95587',
+  bug: '#A6B91A',
+  rock: '#B6A136',
+  ghost: '#735797',
+  dragon: '#6F35FC',
+  dark: '#705746',
+  steel: '#B7B7CE',
+  fairy: '#D685AD'
+}
+
+export const badges = {
+  normal: <NormalBadge />,
+  fire: <FireBadge />,
+  water: <WaterBadge />,
+  electric: <ElectricBadge />,
+  grass: <GrassBadge />,
+  ice: <IceBadge />,
+  fighting: <FightingBadge />,
+  poison: <PoisonBadge />,
+  ground: <GroundBadge />,
+  flying: <FlyingBadge />,
+  psychic: <PsychicBadge />,
+  bug: <BugBadge />,
+  rock: <RockBadge />,
+  ghost: <GhostBadge />,
+  dragon: <DragonBadge />,
+  dark: <DarkBadge />,
+  steel: <SteelBadge />,
+  fairy: <FairyBadge />
+}
